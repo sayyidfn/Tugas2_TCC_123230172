9f29105f83f9c07c4b4f3a69b12f67fa4fd6d38e
Sayyid Fakhri <113014156+sayyidfn@users.noreply.github.com>
2026-05-09 23:23:41 +0700

    Add endpoint to get note by ID

--- routes/notes.js
+++ routes/notes.js
@@ -5,6 +5,7 @@ const noteController = require("../controllers/noteController");
 // Daftar endpoint API Anda
 router.get("/notes", noteController.getNotes);
 router.post("/notes", noteController.createNote);
+router.get("/notes/:id", noteController.getNoteById);
 router.put("/notes/:id", noteController.updateNote);
 router.delete("/notes/:id", noteController.deleteNote);
 
